2a9f5f13b69410b670e981b6faf23036699fe0d1
Luke Siemens <luke@lsiemens.com>
2024-07-08 17:21:57 -0700

    Add MassiveObject to CMakeLists

--- poincare/include/massive_object.hpp
+++ poincare/include/massive_object.hpp
@@ -1,9 +1,14 @@
 #ifndef MASSIVE_OBJECT_H
 #define MASSIVE_OBJECT_H
 
-#include "minkowski.h"
+#include "minkowski.hpp"
+
+#include <memory>
+#include <string>
 
 namespace poincare {
+
+class Sprite;
 
 class MassiveObject {
 public:
@@ -13,18 +18,25 @@ public:
     /// Location of the object in the systems refrence frame.
     minkowski::Point position;
 
-    /// Velocity of the object in the systems refrence frame.
+    /// Velocity of the object in the systems refrence frame. This vector
+    /// should be normalized so velocity*velocity = -1 (using c = 1 units);
     minkowski::Vector velocity;
 
-    /// Orientation of the object as seen in its own refrence frame.
+    /// Orientation of the object as seen in its own refrence frame. For
+    /// now the orientation will not be trated as a kinematic variable
+    /// and will not give it a full relativistic treatment.
     /// TODO relativistic version?
     float orientation;
 
     /// Worldline of the particle in the systems refrance frame.
-    minkowski::Worldline worldline
+    minkowski::Worldline worldline;
 
-    MassiveObject();
+    MassiveObject(minkowski::Point position, minkowski::Vector velocity, std::string sprite_path);
+
+    void Update(double dtau);
 private:
+    /// The sprite to render for this object
+    std::shared_ptr<Sprite> sprite;
 };
 
 } // namespace poincare
--- poincare/src/massive_object.cpp
+++ poincare/src/massive_object.cpp
@@ -1,9 +1,18 @@
 #include "massive_object.hpp"
 
+
+
 namespace poincare {
 
-MassiveObject::MassiveObject() {
+MassiveObject::MassiveObject(minkowski::Point position, minkowski::Vector velocity, std::string sprite_path) : proper_time(0), position(position), velocity(velocity) {
+    worldline.AddPoint(position);
+}
 
+void MassiveObject::Update(double dtau) {
+    position = position + dtau*velocity;
+    proper_time += dtau;
+
+    worldline.AddPoint(position);
 }
 
 } // namespace poincare
